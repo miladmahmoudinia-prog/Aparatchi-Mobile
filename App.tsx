9638647eb59c05f7ee24933056b59b3cb362ff4d
miladmahmoudinia-prog <milad.mahmoudinia@gmail.com>
2026-09-02 11:58:09 +0330

    release: show app version 0.16.10

--- App.tsx
+++ App.tsx
@@ -68,7 +68,7 @@ import {
   syncEpisodeAlerts,
 } from './src/notificationManager';
 
-const APP_DISPLAY_VERSION = '0.16.9';
+const APP_DISPLAY_VERSION = '0.16.10';
 
 type MainTab = 'home' | 'categories' | 'search' | 'favorites' | 'downloads';
 type ScheduleFilter = 'all' | 'iranian' | 'foreign';
